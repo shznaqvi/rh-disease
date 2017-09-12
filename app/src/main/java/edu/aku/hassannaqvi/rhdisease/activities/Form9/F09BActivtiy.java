7a1234bbb63a72badd050ae7ceb1bad3314179e2
gul.sanober <gulsanobermemon@gmail.com>
2017-09-12 09:54:58 +0500

    Modules created

---
+++ app/src/main/java/edu/aku/hassannaqvi/rhdisease/activities/Form9/F09BActivtiy.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.rhdisease.activities.Form9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.rhdisease.R;
+
+public class F09BActivtiy extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f09_bactivtiy);
+    }
+}
