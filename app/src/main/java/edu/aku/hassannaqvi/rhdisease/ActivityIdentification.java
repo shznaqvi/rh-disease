5d21092dd69a34fcb93d2e7b2654107a0cfd16f6
ali.azaz <openmind_ali@hotmail.com>
2017-09-12 10:31:07 +0500

    commit

---
+++ app/src/main/java/edu/aku/hassannaqvi/rhdisease/ActivityIdentification.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.rhdisease;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityIdentification extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identification);
+    }
+}
