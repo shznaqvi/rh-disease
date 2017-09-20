cc44683ad8bf4d24f6f3a1d6da2b3c52d7a14703
shznaqvi <shznaqvi@gmail.com>
2017-09-20 14:25:52 +0500

    Sync fix

--- app/src/main/java/edu/aku/hassannaqvi/rhdisease/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/rhdisease/core/MainApp.java
@@ -35,7 +35,7 @@ public class MainApp extends Application {
 
     public static final String _IP = "43.245.131.159"; // Test PHP server
     public static final Integer _PORT = 8080; // Port - with colon (:)
-    public static final String _HOST_URL = "http://" + MainApp._IP + ":" + MainApp._PORT + "/rhdisease/api/";
+    public static final String _HOST_URL = "http://" + MainApp._IP + ":" + MainApp._PORT + "/rhd/api/";
 
     /*
         public static final String _IP = "43.245.131.159"; // Test server
