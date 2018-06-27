e447852a32f460a19434257dea030760d9b2c937
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-06-27 17:35:36 +0500

    fixed a sync function

--- app/src/main/java/edu/aku/hassannaqvi/rhdisease/core/DatabaseHelper.java
+++ app/src/main/java/edu/aku/hassannaqvi/rhdisease/core/DatabaseHelper.java
@@ -278,6 +278,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 values.put(RH_ResultsTable.COLUMN_GA_DAYS, rh.getGa_days());
                 values.put(RH_ResultsTable.COLUMN_F10_ACCEPTANCE, rh.getF10_acceptance());
                 values.put(RH_ResultsTable.COLUMN_F15_ADVERSE, rh.getF15_adverse());
+                values.put(RH_ResultsTable.COLUMN_F10_UID, rh.getF15_adverse());
+
                 values.put(RH_ResultsTable.COLUMN_F5, rh.getf5());
                 values.put(RH_ResultsTable.COLUMN_F8, rh.getf8());
                 values.put(RH_ResultsTable.COLUMN_F9, rh.getf9());
@@ -1038,6 +1040,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(RH_ResultsTable.COLUMN_FORM5_UID, rh.getForm5_uid());
         values.put(RH_ResultsTable.COLUMN_F10_ACCEPTANCE, rh.getF10_acceptance());
         values.put(RH_ResultsTable.COLUMN_F15_ADVERSE, rh.getF15_adverse());
+        values.put(RH_ResultsTable.COLUMN_F10_UID, rh.getf10_uid());
         values.put(RH_ResultsTable.COLUMN_F5, rh.getf5());
         values.put(RH_ResultsTable.COLUMN_F8, rh.getf8());
         values.put(RH_ResultsTable.COLUMN_F9, rh.getf9());
